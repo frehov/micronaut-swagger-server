f9a54d10960c12dad9ea821f30d5bad7f2efb4cf
Fredrik Hov <fredrik.hov@knowit.no>
2018-10-30 21:20:35 +0100

    Add builders to swaggerconfig, add support for rendering offsite specs, and autogenerate list of available specs from the config

--- src/main/java/com/crappyegineering/ubw/SwaggerConfig.java
+++ src/main/java/com/crappyegineering/ubw/SwaggerConfig.java
@@ -1,14 +1,20 @@
 package com.crappyegineering.ubw;
 
+import java.util.List;
+
 import io.micronaut.context.annotation.ConfigurationProperties;
 
-@ConfigurationProperties("application.api.swagger")
+@ConfigurationProperties(SwaggerConfig.PREFIX)
 public class SwaggerConfig {
 
+    public static final String PREFIX = "application.api.swagger";
+
     private String version;
-    private String path;
     private String layout;
     private boolean deepLinking;
+    private List<URIConfig> urls;
+
+
 
     public String getVersion() {
         return version;
@@ -16,14 +22,6 @@ public class SwaggerConfig {
 
     public void setVersion(String version) {
         this.version = version;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
     }
 
     public String getLayout() {
@@ -40,5 +38,115 @@ public class SwaggerConfig {
 
     public void setDeepLinking(boolean deepLinking) {
         this.deepLinking = deepLinking;
+    }
+
+    public List<URIConfig> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<URIConfig> urls) {
+        this.urls = urls;
+    }
+
+    @Override
+    public String toString() {
+        return "SwaggerConfig{" +
+                "version='" + version + '\'' +
+                ", layout='" + layout + '\'' +
+                ", deepLinking=" + deepLinking +
+                ", specs=" + urls +
+                '}';
+    }
+
+    @ConfigurationProperties(URIConfig.PREFIX)
+    public static class URIConfig {
+
+        static final String PREFIX = "urls";
+
+        private String name;
+        private String url;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public String toString() {
+            return "URIConfig{" +
+                    "name='" + name + '\'' +
+                    ", url='" + url + '\'' +
+                    '}';
+        }
+
+        public static class Builder {
+            private String name;
+            private String url;
+
+            public Builder withName(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public Builder withURI(String url) {
+                this.url = url;
+                return this;
+            }
+
+            public URIConfig build() {
+                URIConfig uriConfig = new URIConfig();
+                uriConfig.setName(name);
+                uriConfig.setUrl(url);
+                return uriConfig;
+            }
+        }
+
+    }
+
+    public static class Builder {
+        private String version;
+        private String layout;
+        private boolean deepLinking;
+        private List<URIConfig> urls;
+
+        public Builder withVersion(String version) {
+            this.version = version;
+            return this;
+        }
+
+        public Builder withLayout(String layout) {
+            this.layout = layout;
+            return this;
+        }
+
+        public Builder withDeepLinking(boolean deepLinking) {
+            this.deepLinking = deepLinking;
+            return this;
+        }
+
+        public Builder withUrls(List<URIConfig> urls) {
+            this.urls = urls;
+            return this;
+        }
+
+        public SwaggerConfig build() {
+            SwaggerConfig swaggerConfig = new SwaggerConfig();
+            swaggerConfig.setVersion(version);
+            swaggerConfig.setLayout(layout);
+            swaggerConfig.setDeepLinking(deepLinking);
+            swaggerConfig.setUrls(urls);
+            return swaggerConfig;
+        }
     }
 }
--- src/main/java/com/crappyegineering/ubw/controller/SwaggerController.java
+++ src/main/java/com/crappyegineering/ubw/controller/SwaggerController.java
@@ -1,18 +1,23 @@
 package com.crappyegineering.ubw.controller;
 
 import static io.micronaut.http.HttpResponse.ok;
+import static java.util.Collections.singletonList;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 
 import com.crappyegineering.ubw.SwaggerConfig;
+import com.crappyegineering.ubw.SwaggerConfig.URIConfig;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.validation.Validated;
 import io.micronaut.views.View;
 import io.swagger.v3.oas.annotations.Hidden;
 
 @Hidden
 @Controller("/api")
+@Validated
 public class SwaggerController {
 
     @Inject
@@ -23,4 +28,21 @@ public class SwaggerController {
     public HttpResponse index() {
         return ok(config);
     }
+
+    @View("swagger/index")
+    @Get("/{url}")
+    public HttpResponse renderSpec(@NotNull String url) {
+        SwaggerConfig tmpConfig = new SwaggerConfig.Builder()
+                .withDeepLinking(config.isDeepLinking())
+                .withLayout(config.getLayout())
+                .withVersion(config.getVersion())
+                .withUrls(singletonList(new URIConfig.Builder()
+                        .withName(url)
+                        .withURI(url)
+                        .build()))
+                .build();
+
+        return ok(tmpConfig);
+    }
+
 }
